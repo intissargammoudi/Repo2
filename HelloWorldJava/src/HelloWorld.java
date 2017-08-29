687672e8c8326236676f31584d9f4e6327ee6a9f
intissargammoudi <intissargm4@gmail.com>
2017-08-29 22:34:23 +0200

    simple change comment

--- HelloWorldJava/src/HelloWorld.java
+++ HelloWorldJava/src/HelloWorld.java
@@ -2,7 +2,7 @@
 public class HelloWorld {
 
     public static void main(String[] args) {
-        // Prints "Hello, World" to the terminal window.
+       //55555/ Prints "Hello, World" to the terminal window.
         System.out.println("Hello, World");
     }
 
