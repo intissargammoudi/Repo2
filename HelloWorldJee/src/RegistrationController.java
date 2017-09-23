e878996ed15b6730f7441a750905ea482f992bc8
intissargammoudi <intissargm4@gmail.com>
2017-09-23 20:40:55 +0200

    feyza change

--- HelloWorldJee/src/RegistrationController.java
+++ HelloWorldJee/src/RegistrationController.java
@@ -16,7 +16,7 @@ import javax.servlet.http.HttpServletResponse;
  */
 public class RegistrationController extends HttpServlet {
 
- protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+ //protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
   // TODO Auto-generated method stub
   response.setContentType("text/html");
   PrintWriter out = response.getWriter();
